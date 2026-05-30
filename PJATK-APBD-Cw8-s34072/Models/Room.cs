554ebc4e041760148126a572dcbe49519b800941
Krzysztof Lisowski <s34072@pjwstk.edu.pl>
2026-05-30 21:16:01 +0200

    Ostateczny commit z rozwiązaniem

--- PJATK-APBD-Cw8-s34072/Models/Room.cs
+++ PJATK-APBD-Cw8-s34072/Models/Room.cs
@@ -1,10 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
 namespace PJATK_APBD_Cw8_s34072.Models;
 
 public partial class Room
 {
     public string Id { get; set; } = null!;
+
     public int WardId { get; set; }
+
     public bool HasTv { get; set; }
-    public virtual Ward Ward { get; set; } = null!;
+
     public virtual ICollection<Bed> Beds { get; set; } = new List<Bed>();
+
+    public virtual Ward Ward { get; set; } = null!;
 }
